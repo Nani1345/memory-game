252ef5c5970663ca4cfda4bb3c79f61225eeafb2
Na <xuna1026000@gmail.com>
2024-07-22 09:57:23 -0700

    add js code about game instruction

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
-console.log('test')
+let imageComputerChoice  
+let imagePlayerChoice 
+const imagesShow = ['', '', '', '', '', '', '', '']
+const imageslab = [../images/]
+const startBtnEl = document.getElementById('startButton')
+const gamInstruction = document.querySelector('.gameIntrocution')
+const imageEl = document.getElementById('imagesContainer')
+
+startBtnEl.addEventListener('click', (event) => {
+    gamInstruction.style.visibility = 'hidden'
+    imageEl.style.visibility = 'visible'
+})
+
